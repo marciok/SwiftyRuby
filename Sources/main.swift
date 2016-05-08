22c79c943e7ae99f17f51bdb19e3669c3d27d031
Marcio Klepacz <marcioklepacz@gmail.com>
2016-05-08 18:10:25 +0200

    Add argument on example

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,13 +3,12 @@ import Foundation
 
 ruby_setup()
 rb_require("./hello_world/hello.rb")
-let argStr = rb_str_new_cstr("Citrous")
-var array: Array<UnsafeMutablePointer<VALUE>> = [argStr]
 
-var result = rb_funcallv(0, rb_intern("hello"), 1, argStr)
+var stringArg = rb_str_new_cstr("Swift")
+var result = rb_funcallv(0, rb_intern("hello"), 1, &(stringArg))
 var str = rb_string_value_cstr(&(result))
-let r = String(cString: str)
-print(r)
+
+print(String(cString: str))
 
 
 ruby_cleanup(0)
