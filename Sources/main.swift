6989e5395b2869242184253991fa16ad43d5ef4a
Marcio Klepacz <marcioklepacz@gmail.com>
2016-05-09 21:08:40 +0200

    Add comments

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,12 @@
 import CRuby
-import Foundation
 
 ruby_setup()
-rb_require("./hello_world/hello.rb")
+rb_require("./hello_world/hello.rb") // Importing Ruby file
 
-var stringArg = rb_str_new_cstr("Swift")
-var result = rb_funcallv(0, rb_intern("hello"), 1, &(stringArg))
-var str = rb_string_value_cstr(&(result))
+var stringArg = rb_str_new_cstr("Swift") // Creating new Ruby string
+var result = rb_funcallv(0, rb_intern("hello"), 1, &(stringArg)) // Calling the function `hello` passing `stringArg`
+var str = rb_string_value_cstr(&(result)) // Extracting the returned value as C string
 
 print(String(cString: str))
 
-
 ruby_cleanup(0)
-
